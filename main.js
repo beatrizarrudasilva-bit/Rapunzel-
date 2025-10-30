e1defabc9cd66806da54f02d8ac4b1be1b56aa2a
beatrizarrudasilva-bit <beatriz.arruda.silva@escola.pr.gov.br>
2025-10-30 12:12:16 -0300

    g

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const avanca = document. querySelectorAll("btn-proximo")
+const avanca = document.querySelectorAAL("btn-proximo")
 console.log(avanca)
 
 avanca.forEach(button =>{
-    button.addEventListener('click', function ()
+    button.addEventListener('click', function (){
   const atual = document.querySelector('.ativo')
 const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 
  atual.classList.remove('.ativo')
  document.getElementById(proximoPasso).classList.add('.ativo')
 }) 
+})
